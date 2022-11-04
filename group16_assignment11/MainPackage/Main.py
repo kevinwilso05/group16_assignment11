bf1902da2303b7a2e87cb8cb206c1d67e3a313cb
bluej <bluej@DESKTOP-2NJ279P>
2022-11-03 20:13:22 -0400

    Added Initial Documentation and API Link

--- group16_assignment11/MainPackage/Main.py
+++ group16_assignment11/MainPackage/Main.py
@@ -1,6 +1,12 @@
 '''
-Created on Nov 3, 2022
-
-@author: KevinWilson
+Name: Kevin Wilson, Brett Perez
+email: perezbd@mail.uc.edu
+Assignment: Assignment 11
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: 
+Citations: 
+https://edan.si.edu/openaccess/apidocs/#api-_
+Anything else that's relevant:
 '''
-# TEST
+# API Link: view-source:https://api.si.edu/openaccess/api/v1.0/search?api_key=egqk7unCsFkJqCiurbvF1x4gNZZyjKJwCIBVt5qw&q=history
