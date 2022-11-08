84587888eebf57bc1df395cb8a5bd5f0ba735578
bluej <bluej@DESKTOP-2NJ279P>
2022-11-07 20:06:50 -0500

    Added code to Request, Receive, Load, and Parse data from server

--- group16_assignment11/MainPackage/Main.py
+++ group16_assignment11/MainPackage/Main.py
@@ -10,3 +10,13 @@ https://edan.si.edu/openaccess/apidocs/#api-_
 Anything else that's relevant:
 '''
 # API Link: view-source:https://api.si.edu/openaccess/api/v1.0/search?api_key=egqk7unCsFkJqCiurbvF1x4gNZZyjKJwCIBVt5qw&q=history
+
+import json
+import requests
+
+response = requests.get('https://api.si.edu/openaccess/api/v1.0/search?api_key=egqk7unCsFkJqCiurbvF1x4gNZZyjKJwCIBVt5qw&q=history')
+json_string = response.content
+    
+parsed_json = json.loads(json_string)
+
+print(parsed_json)
